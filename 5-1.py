5e1c250e8d574991671614a0143135fd2dad03a5
bangsunghwan1102 <83874446+bangsunghwan1102@users.noreply.github.com>
2021-05-09 00:10:10 +0900

    5-1.py

---
+++ 5-1.py
@@ -0,0 +1,13 @@
+# 블리안 : boolean  - 조건에 따라서...
+# 여러줄 주석  Shift + Alt + A
+# 한줄한줄 Ctrl + /
+
+x = True
+y = False
+print(x), print(y)
+print("\n")
+a = 10
+b = 14
+if a > b :
+    print("a가 크다.")
+print("False이기 때문에 출력 안된다.","\n") # a가 b보다 작으므로 출력x
