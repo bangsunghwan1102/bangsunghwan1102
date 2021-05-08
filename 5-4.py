631fe1ed9a5ffda10e98391dea8a8fbd6ae46878
bangsunghwan1102 <83874446+bangsunghwan1102@users.noreply.github.com>
2021-05-09 00:15:54 +0900

    5-4.py

---
+++ 5-4.py
@@ -0,0 +1,16 @@
+# 불리언 4th- 조건문 if else, elif, else
+# 여러줄 주석  Shift + Alt + A
+# 한줄한줄 Ctrl + /
+a = 4
+b = 3
+c = 7
+d = c - a
+
+if a > b :                       # a가 b보다 크면 Hi
+    print("Hi") 
+if a > c:                        # a가 c보다 크면 Hello
+    print("Hello")
+elif a == d:                     # a가 3과 같다면 Bad
+    print("Bad")     
+else:                            # a가 b보다 작으면 Good
+    print("Good")
